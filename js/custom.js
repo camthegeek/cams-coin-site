a737fe34731993289cc8b3229b8c8683d72993e8
camthegeek <cam@camthegeek.net>
2019-05-08 20:12:07 -0400

    Some language stuff

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+// --------------------------------------------------------
+//	Language Dropdown
+// -------------------------------------------------------- 
+$(function() {
+	$('.langbtn').click(function(e){
+    	$('.lang-dropdown-child').addClass('show-child');
+    	e.stopPropagation();
+	})
+	$('body').click(function(){
+    	$('.lang-dropdown-child').removeClass('show-child');
+  	});
+});

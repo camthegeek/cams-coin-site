b7df312869a25a3782023f5e3d2e1834ede67ed2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-14 17:16:31 +0000

    Deploy camthegeek/cams-coin-site to github.com/camthegeek/cams-coin-site.git:gh-pages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 jQuery(function($) {
 	"use strict";
-	// Author Code Here
 
 	var ratio = 2;
 
@@ -39,18 +38,6 @@ jQuery(function($) {
 			speed: 60
 		});
 
-		// Popup Form Init
-		var i = 0;
-		var interval = 0.15;
-		$('.popup-form .dropdown-menu li').each(function() {
-			$(this).css('animation-delay', i + "s");
-			i += interval;
-		});
-		$('.popup-form .dropdown-menu li a').click(function(event) {
-			event.preventDefault();
-			$(this).parent().parent().prev('button').html($(this).html());
-		});
-
 		// Onepage Nav
 		$('.navbar.navbar-fixed-top .navbar-nav').onePageNav({
 			currentClass: 'active',
@@ -75,16 +62,6 @@ jQuery(function($) {
 	// Window Resize
 	$(window).resize(function() {
 		$('header').height($(window).height());
-	});
-
-	// Pricing Box Click Event
-	$('.pricing .box-main').click(function() {
-		$('.pricing .box-main').removeClass('active');
-		$('.pricing .box-second').removeClass('active');
-		$(this).addClass('active');
-		$(this).next($('.box-second')).addClass('active');
-		$('#pricing').css("background-image", "url(" + $(this).data('img') + ")");
-		$('#pricing').css("background-size", "cover");
 	});
 
 	// Mobile Nav
@@ -128,28 +105,16 @@ jQuery(function($) {
 			}
 		}
 	});
-
-	function centerModal() {
-		$(this).css('display', 'block');
-		var $dialog = $(this).find(".modal-dialog"),
-			offset = ($(window).height() - $dialog.height()) / 2,
-			bottomMargin = parseInt($dialog.css('marginBottom'), 10);
-
-		// Make sure you don't hide the top part of the modal w/ a negative margin
-		// if it's longer than the screen height, and keep the margin equal to 
-		// the bottom margin of the modal
-		if (offset < bottomMargin) offset = bottomMargin;
-		$dialog.css("margin-top", offset);
-	}
-
-	$('.modal').on('show.bs.modal', centerModal);
-
-	$('.modal-popup .close-link').click(function(event){
-		event.preventDefault();
-		$('#modal1').modal('hide');
-	});
-
-	$(window).on("resize", function() {
-		$('.modal:visible').each(centerModal);
-	});
+});
+// --------------------------------------------------------
+//	Language Dropdown
+// -------------------------------------------------------- 
+$(function() {
+	$('.langbtn').click(function(e){
+    	$('.lang-dropdown-child').addClass('show-child');
+    	e.stopPropagation();
+	})
+	$('body').click(function(){
+    	$('.lang-dropdown-child').removeClass('show-child');
+  	});
 });
